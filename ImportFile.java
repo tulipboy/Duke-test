445d710cc5e15518f2249fe480e8fe5e9267ebdd
tulip boy <tulip boy>
2016-05-26 15:22:45 +0200

    changed the setup for import and altering the files (still working on it ;-) )

--- ImportFile.java
+++ ImportFile.java
@@ -1,7 +1,9 @@
 package DukeUniversityJava;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Created by tulip on 30/04/2016.
@@ -13,58 +15,29 @@ public class ImportFile {
     public String setPath(String lokatie){
         return path + lokatie;
     }
+
     public String tijdelijk;
-
-
-
+    public static Scanner reader;
+    ArrayList<String> lines = new ArrayList<String>();
 
 
     public void importFile(String fileLokatie){
         File file = new File(fileLokatie);
-        FileInputStream fis = null;
-        BufferedInputStream bis = null;
-        DataInputStream dis = null;
-        int[] getallen = new int[20];
-
         try {
-            fis = new FileInputStream(file);
-
-            // Here BufferedInputStream is added for fast reading.
-            bis = new BufferedInputStream(fis);
-            dis = new DataInputStream(bis);
-
-            // dis.available() returns 0 if the file does not have more lines.
-            while (dis.available() != 0) {
-
-                String stringVanDis = dis.toString();
-                int aantal = 0;
-
-                System.out.println(stringVanDis);
-                for (int i = 0; i < stringVanDis.length() ; i++){
-                    if (stringVanDis.charAt(i) != ' ') {
-                        aantal++;
-                    } else if (stringVanDis.charAt(i) == ' ') {
-                        getallen[aantal] = getallen[aantal]+1;
-                        aantal = 0;
-                    }
-                }
-                // this statement reads the line from the file and print it to
-                // the console.
-                System.out.println(dis.readLine());
-            }
-
-            // dispose all the resources after using them.
-            fis.close();
-            bis.close();
-            dis.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            reader = new Scanner(file);
+        } catch (Exception e){
+            System.out.println("could not import the file");
         }
-        System.out.println(Arrays.toString(getallen));
+     }
 
+        // converting the document to strings to make it more clear to use some methods on them
+    public void convertFile(){
+        while(reader.hasNextLine()){
+            lines.add(reader.nextLine());
+        }
+        for (int i = 0; i < lines.size(); i++){
+            System.out.println(lines.get(i));
+        }
     }
     }
 
